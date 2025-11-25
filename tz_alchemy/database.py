01485e97670432931f7db4702440e2782ede0e87
Askerov <askerovasif1993@mail.ru>
2025-11-25 04:01:50 +0300

    -

--- tz_alchemy/database.py
+++ tz_alchemy/database.py
@@ -1,6 +1,6 @@
 
 from sqlalchemy.orm import sessionmaker , DeclarativeBase
-from config import host, password, user
+from config import host, password, user, port
 from basse_tz import new_base
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,14 +8,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 # асинхронный
 
-async_engine = create_async_engine(f"postgresql+asyncpg://{user}:{password}@{host}/{new_base}")
-
-
+async_engine = create_async_engine(
+    f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{new_base}",
+    echo=False,  # для дебага SQL запросов
+    pool_pre_ping=True  # проверка соединения
+)
 class Base(DeclarativeBase):
     pass
 
 
 async_session = sessionmaker(async_engine, class_= AsyncSession, expire_on_commit=False)
+
+
+
 
 
 
